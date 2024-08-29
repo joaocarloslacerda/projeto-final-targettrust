82b91fc05e82dbc9f6b90a48013d2054ac33d405
juniorlacerda <juniorlacerdasilva@gmail.com>
2024-08-29 15:20:28 -0300

    feat: adiciona personalização para a pagina NotFound

--- src/Page/NotFound/index.tsx
+++ src/Page/NotFound/index.tsx
@@ -1,1 +1,6 @@
-export const NotFound = () => <h1>Página não encontrada</h1>
+export const NotFound = () => (
+  <div className="flex flex-col gap-4 items-center pt-24">
+    <p className="text-8xl">404</p>
+    <h1 className="font-bold text-4xl bg-gray-300 p-6 rounded-2xl shadow-2xl shadow-slate-950">Página não encontrada</h1>
+  </div>
+)
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,8 +8,8 @@ export const AppRoutes = () => {
         <Routes>
             <Route path="/" element={<Home />}/>
             <Route path="/departamentos" element={<Departamentos/>}/>
-            <Route path="/departamentos/new" element={<Departamentos/>}/>
-            <Route path="/departamentos/edit/:id" element={<Departamentos/>}/>
+            <Route path="/departamentos/new" element={<h1>Incluir Departamento</h1>}/>
+            <Route path="/departamentos/edit/:id" element={<h1>Editar Departamento</h1>}/>
             <Route path="*" element={<NotFound />}/>
         </Routes>
   )
