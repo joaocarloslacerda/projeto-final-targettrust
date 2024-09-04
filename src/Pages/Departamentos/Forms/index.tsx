b8d304326c9cfb6bf35b5b9db2eed5a99bfd6ab8
juniorlacerda <juniorlacerdasilva@gmail.com>
2024-09-03 23:01:07 -0300

    feat: adiciona validação de preenchimento dos campos nome e sigla no momento de uma nova inclusão

--- src/Pages/Departamentos/Forms/index.tsx
+++ src/Pages/Departamentos/Forms/index.tsx
@@ -1,8 +1,25 @@
 import { InputText } from 'primereact/inputtext';
 import { Button } from 'primereact/button';
 import { FloatLabel } from 'primereact/floatlabel';
+import { Message } from 'primereact/message';
+import { useState } from 'react';
 
 export const FormDepartamentos = () => {
+
+  const [inputNome, setNome] = useState('');
+  const [inputSigla, setSigla] = useState('');
+
+  const handleCkick = () => {
+    if(!inputNome){
+      alert("nome não preenchido");
+      // <Message severity='error' text='nome não preenchido'/>
+    }
+    if(!inputSigla){
+      alert("sigla não preenchida");
+      // <Message severity='error' text='sigla não preenchida'/>
+    }
+  }
+
   return (
     <>
       <div className='col-span-12 pb-4'>
@@ -12,17 +29,17 @@ export const FormDepartamentos = () => {
       <div className='col-span-12 grid-rows-2 grid grid-cols-12 gap-2' >
         <div className='col-span-6'>
           <FloatLabel>
-            <InputText className='w-full' id='name'/>
+            <InputText className='w-full' id='name' value={inputNome} onChange={(e) => setNome(e.target.value)}/>
             <label htmlFor="name">Nome</label>
           </FloatLabel>
         </div>
         <div className='col-span-2'>
           <FloatLabel>
-            <InputText className='w-full' id='acronym'/>
+            <InputText className='w-full' id='acronym' value={inputSigla} onChange={(e) => setSigla(e.target.value)}/>
             <label htmlFor="acronym">Sigla</label>
           </FloatLabel>
         </div>
-        <Button className='row-start-2 col-span-4' icon='pi pi-save' severity='success' label='Salvar'/>
+        <Button className='row-start-2 col-span-4' icon='pi pi-save' severity='success' label='Salvar' onClick={handleCkick}/>
         <Button className='row-start-2 col-span-4' icon='pi pi-times' severity='danger' label='Cancelar'/>
       </div>
     </>
