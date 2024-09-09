5cf8bc38d1d3a59cf8937f1ccaa94172be5409e4
juniorlacerda <juniorlacerdasilva@gmail.com>
2024-09-09 00:25:51 -0300

    feat: implementa validação do formulário de cadastro de departamento

--- src/Pages/Departamentos/Forms/index.tsx
+++ src/Pages/Departamentos/Forms/index.tsx
@@ -3,27 +3,43 @@ import { Button } from 'primereact/button';
 import { FloatLabel } from 'primereact/floatlabel';
 import { Message } from 'primereact/message';
 import { useState } from 'react';
+import { Navigate, useNavigate } from 'react-router-dom';
 
 export const FormDepartamentos = () => {
 
+  const navigate = useNavigate();
+
   const [inputNome, setNome] = useState('');
   const [inputSigla, setSigla] = useState('');
+  const [temErroNome, setTemNome] = useState(false);
+  const [temErroSigla, setTemSigla] = useState(false);
+
 
   const handleCkick = () => {
     if(!inputNome){
-      alert("nome não preenchido");
-      // <Message severity='error' text='nome não preenchido'/>
+      setTemNome(true);
     }
     if(!inputSigla){
-      alert("sigla não preenchida");
-      // <Message severity='error' text='sigla não preenchida'/>
+      setTemSigla(true);
     }
   }
 
   return (
     <>
-      <div className='col-span-12 pb-4'>
-        <h1>Departamentos</h1>
+      <div className='col-span-12'>
+        <div className='flex justify-between items-center my-6'>
+          <h1 className='text-2xl font-bold'>Cadastro de Departamentos</h1>
+            <Button
+              icon='pi pi-chevron-left'
+              label='Voltar'
+              severity='info'
+              rounded
+              onClick={() => {
+                navigate('/departamentos');
+              }}
+            />
+        </div>
+          
       </div>
 
       <div className='col-span-12 grid-rows-2 grid grid-cols-12 gap-2' >
@@ -32,12 +48,18 @@ export const FormDepartamentos = () => {
             <InputText className='w-full' id='name' value={inputNome} onChange={(e) => setNome(e.target.value)}/>
             <label htmlFor="name">Nome</label>
           </FloatLabel>
+          <small className='text-red-600' hidden={!temErroNome}>
+            Nome inválido
+          </small>
         </div>
         <div className='col-span-2'>
           <FloatLabel>
             <InputText className='w-full' id='acronym' value={inputSigla} onChange={(e) => setSigla(e.target.value)}/>
             <label htmlFor="acronym">Sigla</label>
           </FloatLabel>
+          <small className='text-red-600' hidden={!temErroSigla}>
+            Sigla inválida
+          </small>
         </div>
         <Button className='row-start-2 col-span-4' icon='pi pi-save' severity='success' label='Salvar' onClick={handleCkick}/>
         <Button className='row-start-2 col-span-4' icon='pi pi-times' severity='danger' label='Cancelar'/>
